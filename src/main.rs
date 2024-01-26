9349fb17bfec5201b5e1ad65ba8738ee0e6efbb6
David McCaffrey <redx@MKVIII.local>
2024-01-25 23:02:31 -0600

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(name = "worsen", version = "0.1.0", author = "David McCaffrey", about = "It's better than bad, it's good!")]
+pub struct Args {
+    /// Operations to apply to the input images
+    #[clap(long, short = 'o', required = true)]
+    operations: Vec<String>,
+
+    /// Input images
+    #[clap(required = true)]
+    files: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("It's better than bad, it's good!");
+    let args= Args::parse();
+    println!("Making things worse: operations={:?}, files={:?}", args.operations, args.files);
 }
