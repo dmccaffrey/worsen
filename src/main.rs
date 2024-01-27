1d746ca70e3f1022727cb8ff3cf9ad3fb6b85e81
David McCaffrey <redx@MKVIII.local>
2024-01-27 12:29:45 -0600

    Add image loading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::path::PathBuf;
+use image::io::Reader as IR;
 
 #[derive(Debug, Parser)]
 #[clap(name = "worsen", version = "0.1.0", author = "David McCaffrey", about = "It's better than bad, it's good!")]
@@ -16,4 +17,15 @@ pub struct Args {
 fn main() {
     let args= Args::parse();
     println!("Making things worse: operations={:?}, files={:?}", args.operations, args.files);
+    let mut images : Vec<image::Rgb32FImage> = vec![];
+    for path in args.files.iter() {
+        let img = load_and_decode_img(&path).into_rgb32f();
+        println!("Loaded image: img={:?}, dimensions={:?}", path, img.dimensions());
+        images.push(img);
+    }
+}
+
+fn load_and_decode_img(path: &PathBuf) -> image::DynamicImage {
+    let img =  IR::open(path).expect("Valid image path");
+    img.decode().expect("Supported image format")
 }
